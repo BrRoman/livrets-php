b706353fe89753b49798d9e0ca786f2ca8df0f01
BrRoman <editeur@traditions-monastiques.com>
2018-09-06 20:50:52 +0200

    Readings added (September, pa_23-26).

--- Data/Lectures/duplicate_feries.sh
+++ Data/Lectures/duplicate_feries.sh
@@ -5,12 +5,12 @@
 #+ et en vidant le fichier.
 
 cd "/media/fr_romain/FR_ROMAIN/Sites/livrets/Data/Lectures/"
-find . -name 'pa_14_*_1_2*' | sort | while read
-#find . -name 'pa_14_6_ev*' | sort | while read
+#find . -name 'pa_14_*_1_2*' | sort | while read
+find . -name 'pa_14_6_ev*' | sort | while read
 do
 	f=`echo "$REPLY"`
-    fn=`echo "$REPLY" | sed 's/pa_14_\(.\)_1_2/pa_21_\1_1_2/g'`
-    #fn=`echo "$REPLY" | sed 's/pa_14_6_ev/pa_21_2_ev/g'`
+    #fn=`echo "$REPLY" | sed 's/pa_14_\(.\)_1_2/pa_26_\1_1_2/g'`
+    fn=`echo "$REPLY" | sed 's/pa_14_6_ev/pa_26_1_ev/g'`
 	#echo "$fn"
 	cp "$f" "$fn"
 	echo "" > "$fn"
