c2a09ba7b58f5023f34fa68307f851c50faca4f2
BrRoman <editeur@traditions-monastiques.com>
2018-11-04 09:13:19 +0100

    Bug fixed: saturdays per annum in nov. and dec.

--- calculate.php
+++ calculate.php
@@ -144,7 +144,7 @@ function calculate_tempo($timestamp){
     // Temps per Annum après la Pentecôte :
     if($timestamp > $pentecote and $timestamp < $current_adv){
         $days_before_current_adv = ceil(($current_adv - $timestamp) / $day);
-        $dim_per_annum = 35 - ceil(($days_before_current_adv - 1)/ 7);
+        $dim_per_annum = 35 - ceil(($days_before_current_adv - 1)/ 7) - (($weekday == 6 and in_array(date('m', $timestamp), [11, 12])) ? 1 : 0); // Après le changement d'heure (donc en novembre et décembre), il faut encore enlever 1 le samedi.
         $tempo = "pa_".$dim_per_annum."_".$weekday;
     }
 
