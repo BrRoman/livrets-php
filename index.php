015a1a2a979923506bcad3a75a0cf040eaadaf56
BrRoman <editeur@traditions-monastiques.com>
2019-05-19 21:27:51 +0200

    Restructuration; formatting; bootstrap, less etc.

--- index.php
+++ index.php
@@ -2,62 +2,80 @@
 <html>
 	<head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="jquery-ui.css">
-        <link rel="stylesheet" href="livrets.css">
-        <script src="jquery-3.2.1.js"></script>
-        <script src="jquery-ui.js"></script>
-        <script src="latex.js"></script>
-        <script src="livrets.js"></script>
+        <link rel="stylesheet" href="static/jquery-ui-1.12.1/jquery-ui.min.css">
+        <link rel="stylesheet" href="static/fontawesome-free-5.7.1-web/css/all.min.css">
+        <link rel="stylesheet" href="static/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet/less" type="text/css" href="static/css/livrets.less">
+        <style>@font-face{font-family: "LucidaHW"; src: url("static/fonts/TR Lucida Handwriting Italic.ttf")}</style>
         <title>Livrets retraites</title>
 	</head>
 	<body>
-        <div id="page">
-            <div id="data">
+        <div id="page" class="d-flex flex-column justify-content-center align-items-center">
+            <div id="data" class="col-lg-7 d-flex flex-column align-items-center">
                 <h1>†</h1>
                 <h2>
-                Livrets de messe</br>
-                pour les retraitants
+                Livret de messe
                 </h2>
 
-                <div id="input">
-                    <label for="date_debut">Retraite du : </label><input id="date_debut" type="text" name="date_debut">
+                <div id="input" class="row justify-content-center">
+                    <table>
+                        <tr>
+                            <td><input type="radio" name="type" value="type_jour" id="radio_jour">
+                            <label for="date_jour">Jour : </label></td><td><input id="date_jour" type="text" name="date_jour" disabled></td>
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="type" value="type_retraite" id="radio_retraite" checked>
+                            <label for="date_retraite">Retraite du : </label></td><td><input id="date_retraite" type="text" name="date_retraite"></td>
+                        </tr>
+                    </table>
                 </div>
 
-                <div id="output">
-                    <?php
-                        $tab_grid = array("IN", "GR", "AL", "OF", "CO", "KY", "GL", "SA", "CR", "TI");
-                        for($i = 0; $i < 5; $i++){
-                            $code = "";
-                            //Jour civil :
-                            $code = $code."<div class='civil_day' id='civil_day_".$i."'></div>";
-                            
-                            // Grille des pièces :
-                            $code = $code."<table class='grid'>";
-                            $code = $code."<tr>";
-                            for($j = 0; $j < 9; $j++){
-                                $code = $code."<td class='grid_elem'><div class='label_cap' id='grid_label_".$i.$j."'>".$tab_grid[$j]."</div><input type='text' id='grid_value_".$i.$j."' style='width:30px'></input></td>";
-                            }
-                            $code = $code."<td class='grid_elem'><div class='label_cap' id='grid_label_".$i."'>".$tab_grid[9]."</div><select id='tierce_page_".$i."'><option value=''></option><option value='2'>Dimanche</option><option value='4'>Lundi</option><option value='6'>Semaine I</option><option value='9'>Mardi II</option><option value='12'>Mercredi II</option><option value='15'>Vendredi II</option><option value='17'>Samedi II</option></select></td>";
-                            $code = $code."</tr>";
-                            $code = $code."</table>";
-                            print($code);}
-                    ?>
+                <div class="d-flex flex-column justify-content-center align-items-center">
+                    <div id="output">
+                        <?php
+                            $tab_grid = array("IN", "GR", "AL", "OF", "CO", "KY", "GL", "SA", "CR", "TIERCE");
+                            for($i = 0; $i < 5; $i++){
+                                $code = "<div id='line_".$i."'>";
+                                //Jour civil :
+                                $code = $code."";
+                                
+                                // Grille des pièces :
+                                $code = $code."<table>";
+                                $code = $code."<thead><tr><th class='civil_day' id='civil_day_".$i."' colspan='9'></th></tr></thead><tbody class='grid d-flex justify-content-center align-items-center'><tr>";
+                                for($j = 0; $j < 9; $j++){
+                                    $code = $code."<td class='grid_elem'><div class='label_cap' id='grid_label_".$i.$j."'>".$tab_grid[$j]."</div><input type='text' id='grid_value_".$i.$j."'></input></td>";
+                                }
+                                $code = $code."<td class='grid_elem'><div class='label_cap' id='grid_label_".$i."'>".$tab_grid[9]."</div><select id='tierce_page_".$i."'><option value=''></option><option value='2'>Dimanche</option><option value='4'>Lundi</option><option value='6'>Semaine I</option><option value='9'>Mardi II</option><option value='12'>Mercredi II</option><option value='15'>Vendredi II</option><option value='17'>Samedi II</option></select></td>";
+                                $code = $code."</tr></tbody>";
+                                $code = $code."</table></div>";
+                                print($code);}
+                        ?>
+                    </div>
                 </div>
             </div>
             <div id="tex">
-                <textarea id="tex_area"></textarea>
-                <input type="button" id="go" value="Voir le pdf" style="width:300px; font-size:18px"></input>
+                <div class="d-flex flex-column align-items-center">
+                    <textarea id="tex_area" class="align-self-center mt-5 mb-5"></textarea>
+                    <input type="button" id="go" class="btn btn-primary" value="Voir le pdf"></input>
+                </div>
             </div>
             <div id="overlay_wait">
-                <img src="wait.gif" style="margin-bottom:20px"></br>
+                <img src="static/images/wait.gif" style="margin-bottom:20px"></br>
                 <p>Votre livret est en cours de préparation…</p>
                 <p>Veuillez patienter quelques instants…</p>
             </div>
-            <form id="overlay_download" action="livret.pdf" target="_blank">
+            <form id="overlay_download" action="livret/livret.pdf" target="_blank">
                 <p>Votre livret est prêt !</p>
                 <input id="view" type="submit" value="Afficher" style="margin-top:20px">
             </form>
         </div>
+
+        <script src="static/jquery-3.3.1.min.js"></script>
+        <script src="static/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+        <script src="static/bootstrap-4.0.0-dist/js/bootstrap.bundle.min.js"></script>
+        <script src="static/js/less.min.js"></script>
+        <script src="static/js/latex.js"></script>
+        <script src="static/js/livrets.js"></script>
 	</body>
 </html>
 
---
+++ static/js/livrets.js
@@ -0,0 +1,154 @@
+$(document).ready(function(){
+    // Lier le datepicker à l'input date :
+    $(function(){
+        var values = {
+            dateFormat: "dd/mm/yy",
+            minDate: null,
+            dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+            dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+            monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+            monthNamesShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Juin", "Juil", "Aoû", "Sep", "Oct", "Nov", "Déc"]
+        }
+        $("#date_jour").datepicker(values);
+        $("#date_retraite").datepicker(values);
+    });
+
+    // Activer ou désactiver les input date en fonction du radio cliqué :
+    $("#radio_jour").click(function(){
+        $("#date_jour").attr("disabled", false);
+        $("#date_retraite").attr("disabled", true);
+        $("#date_retraite").val("");
+    });
+    $("#radio_retraite").click(function(){
+        $("#date_retraite").attr("disabled", false);
+        $("#date_jour").attr("disabled", true);
+        $("#date_jour").val("");
+    });
+
+    // Quand une date est sélectionnée, rafraîchir le formulaire et mettre à jour le LaTeX :
+    $("#date_jour").change(function(){
+        $("#output").css("display", "block");
+        $("#line_1").css("display", "none");
+        $("#line_2").css("display", "none");
+        $("#line_3").css("display", "none");
+        $("#line_4").css("display", "none");
+        $("#tex").css("display", "block");
+        update();
+    });
+    $("#date_retraite").change(function(){
+        $("#output").css("display", "block");
+        $("#line_1").css("display", "block");
+        $("#line_2").css("display", "block");
+        $("#line_3").css("display", "block");
+        $("#line_4").css("display", "block");
+        $("#tex").css("display", "block");
+        update();
+    });
+
+    // Quand un chant 'pièce' (Introït, Kyrie, etc.) est modifié, mettre à jour le LaTeX :
+    for(var i = 0; i < 5; i++){
+        for(var j = 0; j < 9; j++){
+            $("#grid_value_" + i + j).keyup(function(){
+                var page = $(this).val();
+                page = page.replace(".", ",");
+                $(this).val(page);
+                update();
+            });
+        }
+        $("#tierce_page_" + i).change(function(){
+            update();
+        });
+    }
+
+    // Fonctions associées au clic sur les boutons "Compiler le pdf" et "Voir le pdf" des 2 overlays :
+    $("#go").click(function(){
+        $("#overlay_wait").css("display", "flex");
+        $.post(
+        "script.php",
+        {"tex": $("#tex_area").val()},
+        function(retour){
+            console.log("Back from compilation:", retour);
+            $("#overlay_wait").css("display", "none");
+            $("#overlay_download").css("display", "flex");
+        },
+        "text"
+        );
+    });
+    $("#view").click(function(){
+        $("#overlay_download").css("display", "none");
+    });
+});
+
+function update(){
+    var days_fr = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    var months_fr = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+    var data = {};
+    if($("#radio_retraite").prop("checked")){
+        var start_date_split = $("#date_retraite")[0].value.split("/");
+        var start_date = new Date(start_date_split[2], start_date_split[1] - 1, start_date_split[0]).getTime();
+        for(var i = 0; i < 5; i++){
+            var date_timestamp = start_date + ((i + 1) * 24 * 3600 * 1000);
+            var date = new Date(date_timestamp);
+
+            // Update jour civil :
+            var weekday = days_fr[date.getDay()];
+            var num_day = date.getDate();
+            if(num_day == 1){
+                num_day = "1er";
+            }
+            var month = months_fr[date.getMonth()];
+            var year = date.getFullYear();
+            var civil_day = weekday + " " + num_day + " " + month + " " + year + " :";
+            $("#civil_day_" + i).text(civil_day);
+            
+            // Mise en objet de toutes les données du formulaire :
+            var grid = {};
+            grid["timestamp"] = date_timestamp;
+            for(var j = 0; j < 9; j++){
+                grid[$("#grid_label_" + i + j).text()] = $("#grid_value_" + i + j).val();
+            }
+            grid["tierce_page"] = $("#tierce_page_" + i).val();
+            data[i] = grid;
+        }
+    }
+    else if($("#radio_jour").prop("checked")){
+        var date_split = $("#date_jour")[0].value.split("/");
+        var date_timestamp = new Date(date_split[2], date_split[1] - 1, date_split[0]).getTime();
+        var date = new Date(date_timestamp);
+
+        // Update jour civil :
+        var weekday = days_fr[date.getDay()];
+        var num_day = date.getDate();
+        if(num_day == 1){
+            num_day = "1er";
+        }
+        var month = months_fr[date.getMonth()];
+        var year = date.getFullYear();
+        var civil_day = weekday + " " + num_day + " " + month + " " + year + " :";
+        $("#civil_day_0").text(civil_day);
+        
+        // Mise en objet de toutes les données du formulaire :
+        var grid = {};
+        grid["timestamp"] = date_timestamp;
+        for(var i = 0; i < 9; i++){
+            grid[$("#grid_label_0" + i).text()] = $("#grid_value_0" + i).val();
+        }
+        grid["tierce_page"] = $("#tierce_page_0").val();
+        data[0] = grid;
+    }
+    request(data);
+}
+
+function request(data_json){
+    console.log("js =", data_json);
+    $.post(
+        "request.php",
+        data_json,
+        function(data){
+            console.log("php =", data);
+            write_latex(data);
+        },
+        "json"
+    );
+}
+
