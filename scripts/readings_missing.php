a4248a5c066f6aebb5d5aa06e64ac1bb22cec320
BrRoman <editeur@traditions-monastiques.com>
2019-06-12 20:56:37 +0200

    Script readings_missing.php: minor corr.

--- scripts/readings_missing.php
+++ scripts/readings_missing.php
@@ -1,20 +1,20 @@
 <?php
-    // This script writes in file "missing_readings" the list of missing readings.
+    // This script writes in file "readings_missing" the list of missing readings.
     // It has to be put in the readings' dir.
 
-    if(is_file(__DIR__."/missing_readings")){
+    if(is_file(__DIR__."/readings_missing")){
         // Empty the result file:
-        system("echo '' > ".__DIR__."/missing_readings");
+        system("echo '' > ".__DIR__."/readings_missing");
     }
 
-    $f = fopen(__DIR__."/missing_readings", "w");
+    $f = fopen(__DIR__."/readings_missing", "w");
 
     // Readings of feriæ per annum:
     for($i = 1; $i <= 34; $i++){
         for($j = 1; $j <= 6; $j++){
             for($k = 1; $k <= 2; $k++){
                 $file = "pa_${i}_${j}_1_${k}.txt";
-                if(!is_file(__DIR__."/$file")){
+                if(!is_file(__DIR__."/../static/data/lectures/$file")){
                     fwrite($f, $file."\n");
                 }
             }
